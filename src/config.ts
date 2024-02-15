dcf5b88c3b12171a9ae8be17528e6829b859bb47
SAIGUSA Tomotada <31317056+10mocy@users.noreply.github.com>
2024-02-15 20:04:26 +0900

    仮

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+const config = {
+    siteUrl: 'https://futabastep.net',
+    copyright: {
+      year: 2024,
+      author: 'えいちせ準備会'
+    },
+    event: {
+      name: 'AHSession',
+      date: '2024-07-21',
+      place: '名古屋・ポートメッセなごや',
+      content: 'AHSキャラクターオンリー即売会',
+      twitterId: 'jambandsession'
+    },
+    shouldShowNectaritionLogo: false
+  }
+  
+  export default config
